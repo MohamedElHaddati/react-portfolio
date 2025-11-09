744c57ee57553f7e3ddaae7ddd3f0cc0427b6ba8
MohamedElHaddati <mohamed.elhaddati@gmail.com>
2025-11-09 13:09:49 +0100

    Added first loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import './index.css';
+import { LoadingScreen } from './components/LoadingScreen';
 
 function App() {
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
-    <>
-      
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+    <div className='min-h-screen transition-opacity duration-700'>
+
+    </div>
     </>
-  )
+  );
 }
 
 export default App
