aa181fa63e0a9f2d69f7aa66997ccfcb2d506f01
MohamedElHaddati <mohamed.elhaddati@gmail.com>
2025-11-09 15:51:51 +0100

    Added navbar and hamburger menu for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react';
 import './App.css';
 import './index.css';
 import { LoadingScreen } from './components/LoadingScreen';
+import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
 
 function App() {
 
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-    <div className='min-h-screen transition-opacity duration-700'>
-
+    <div className={`min-h-screen transition-opacity duration-700 ${ isLoaded ? "opacitity-100" : "opacity-0" }
+        background-black text-gray-100`}>
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </div>
     </>
   );
